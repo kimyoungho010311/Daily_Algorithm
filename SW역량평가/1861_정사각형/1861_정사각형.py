54d8b3329ac719902eccddf4e12bf0ab26b25abd
김영호 <ohhunmi24@gmail.com>
2026-03-10 10:33:52 +0900

    [SOLVE] 1861_정사각형 풀이 완료

--- SW역량평가/1861_정사각형/1861_정사각형.py
+++ SW역량평가/1861_정사각형/1861_정사각형.py
@@ -8,22 +8,6 @@ N * N형태의 방
 물론 이동하려는 방이 존재해야하고, 이동하려는 방에 적힌 숫자가 현재 방에 적힌 숫자보다 정확히 1 더 커 야한다.
 처음에 어떤 수가 적힌 방에서 있어야 가장 많은 개수의 방을 이동할 수 있는지 구하는 프로그램을 작성
 """
-from collections import deque
-
-def BFS(i, j):
-    move_cnt = 1
-    q = deque([(i, j, move_cnt)])
-    visited[i][j] = True
-    while q:
-        curr_i, curr_j, move_cnt = q.popleft()
-
-        for k in range(4):
-            ni, nj = curr_i + di[k], curr_j + dj[k]
-            if 0 <= ni < N and 0 <= nj < N and not visited[ni][nj] and room[curr_i][curr_j] + 1 == room[ni][nj]:
-                visited[ni][nj] = True
-                q.append((ni, nj, move_cnt + 1))
-    return move_cnt
-
 # 상 하 좌 우
 di = [-1, 1, 0, 0]
 dj = [0, 0, -1, 1]
@@ -33,25 +17,33 @@ T = int(input())
 for tc in range(1, T+1):
     N = int(input())
 
+    can_go = [0] * (N*N + 1) # 다음 숫자로 갈 수 있는지 체크하는 배열
     room = []
-    results = []
-    max_move_cnt = -1
 
     for _ in range(N):
         tmp = list(map(int, input().split()))
         room.append(tmp)
 
-    # 모든 원소를 순회하면서
-    # BFS 알고리즘 적용하면 끝날거같은데..?
-
     for i in range(N):
         for j in range(N):
-            visited = [[False] * N for _ in range(N)]
+            curr = room[i][j]
+            for k in range(4):
+                ni, nj = i + di[k], j + dj[k]
+                if 0 <= ni < N and 0 <= nj < N:
+                    if curr + 1 == room[ni][nj]:
+                        can_go[curr] = 1
 
-            move_cnt = BFS(i, j)
+    max_len, curr_len, final_start = 0, 0, 0
 
-            if max_move_cnt < move_cnt:
-                max_move_cnt = move_cnt
-                results.append((room[i][j], max_move_cnt))
-    results.sort(key=lambda x:x[1], reverse=True)
-    print(f"#{tc} {results[0][0]} {results[0][1]}")
+    for i in range(N*N, -1, -1):
+        if i > 0 and can_go[i] == 1:
+            curr_len += 1
+        else:
+            # 1이 끊기는 순간(또는 시작점), 지금까지의 길이 체크
+            # curr_len + 1을 하는 이유는 '이동 횟수'가 아니라 '방의 개수'이기 때문
+            if curr_len >= max_len:
+                max_len = curr_len
+                final_start = i + 1# 연속이 시작된 숫자
+            curr_len = 0
+
+    print(f"#{tc} {final_start} {max_len + 1}")
