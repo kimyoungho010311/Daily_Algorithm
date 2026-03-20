d92dc1a131608ee25030f296728709589ce0907a
김영호 <ocean010311@gmail.com>
2026-03-20 16:36:20 +0900

    [REFACTOR] 문코드 최적화

--- SW역량평가/5658_보물상자비번/5658_보물상자비번.py
+++ SW역량평가/5658_보물상자비번/5658_보물상자비번.py
@@ -1,62 +1,29 @@
-
-"""
-0 1 2 3 4 5 6 7 8 9 A B C D E F
-
-뚜겅은 시계방향으로 돌릴 수 있고, 한 번 돌릴 때마다 숫자가 시계방향으로 한 칸씩 회전한다.
-
-int(16진수문자열, 16)
-중복되는게 생길 수 있으니깐 set으로 한 다음에 정렬하자.
-
-2차원 리스트로 pop push 하면 편할 듯?
--> 아니다 리스트 4개 만들고 큐로 하면 될듯..?
-"""
 import sys
 sys.stdin = open('input.txt')
 
 from collections import deque
 
 T = int(input())
-# T = 1
+
 for tc in range(1, T+1):
-    # 숫자 수, 번재 큰 수
     N, K = map(int, input().split())
-    nums = list(map(str, input().strip()))
 
-    n = len(nums)
-    group_size = n // 4
-    groups = []
+    nums = deque(input().strip())
 
-    for idx, i in enumerate(range(0, n, group_size)):
-        groups.append(nums[i : i + group_size]) #
+    group_size = N // 4
+    ans_set = set()
 
-    q0 = deque(groups[0])
-    q1 = deque(groups[1])
-    q2 = deque(groups[2])
-    q3 = deque(groups[3])
+    for _ in range(group_size):
+        # 현재 상태에서 4개 변의 숫자 추출
+        for i in range(0, N, group_size):
+            # 큐를 리스트로 바꿔서 필요한 만큼만 join
+            hex_str = "".join(list(nums)[i : i + group_size])
+            ans_set.add(hex_str)
 
-    ans = []
-    for i in range(group_size):
-        tmp = q3.pop()
+        nums.rotate(1)
 
-        q3.appendleft(q2.pop())
-        q2.appendleft(q1.pop())
-        q1.appendleft(q0.pop())
-        q0.appendleft(tmp)
+    result = sorted([int(x, 16) for x in ans_set], reverse=True)
 
-        qs = [q0, q1, q2, q3]
+    print(f"#{tc} {result[K-1]}")
 
-        for q in qs:
-            tmp = ''
-            for elem in q:
-                tmp += elem
-            if tmp not in ans:
-                ans.append(tmp)
-    # print(ans)
-    final = []
-    for elem in ans:
-        final.append(int(elem, 16))
-
-    final.sort(reverse=True)
-    # print(final)
-    print(f"#{tc} {final[K-1]}")
 
