d8dec282665854c067a288ab02f5200c6847d091
김영호 <ocean010311@gmail.com>
2026-03-24 13:11:48 +0900

    [SOLVE] ㄴã네트워크

---
+++ programmers/DFS_BFS/네트워크.py
@@ -0,0 +1,17 @@
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+
+    def DFS(v):
+        visited[v] = True
+
+        for i in range(n):
+            if computers[v][i] == 1 and not visited[i]:
+                DFS(i)
+
+    for i in range(n):
+        if not visited[i]:
+            DFS(i)
+            answer += 1
+
+    return answer
