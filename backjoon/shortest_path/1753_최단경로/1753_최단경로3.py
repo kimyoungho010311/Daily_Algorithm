a0927d06afe2c2f994c8a173eaf826f520f2aab6
김영호 <ocean010311@gmail.com>
2026-04-15 10:45:32 +0900

    나무높이, 최단경로 복습 완료

---
+++ backjoon/shortest_path/1753_최단경로/1753_최단경로3.py
@@ -0,0 +1,43 @@
+import sys, heapq
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+def dijkstra(start):
+    pq = []
+    heapq.heappush(pq, (0, start))
+    distance[start] = 0
+
+    while pq:
+        dist, curr_node = heapq.heappop(pq)
+
+        if distance[curr_node] < dist:
+            continue
+
+        for next_node, cost in graph[curr_node]:
+            new_cost = dist + cost
+
+            if distance[next_node] > new_cost:
+                distance[next_node] = new_cost
+                heapq.heappush(pq, (new_cost, next_node))
+
+# 정점 수, 간선 수
+V, E = map(int, input().split())
+# 시작 번호
+K = int(input())
+
+graph = [[] for _ in range(V + 1)]
+
+INF = float('inf')
+distance = [INF] * (V + 1)
+
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append([v, w])
+
+dijkstra(K)
+
+for i in range(1, V+1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
